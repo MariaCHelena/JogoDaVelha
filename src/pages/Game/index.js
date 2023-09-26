6e5a097879fd4e2088f69832c9e913c98f6c7632
JoaoSchmitz <jmatheus12349@gmail.com>
2023-09-25 21:34:59 -0300

    corrigindo bugs e formatando código

--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -1,33 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import './style.css';
-import Button from '../../components/Button';
+import React, { useState } from "react";
+import "./style.css";
+import Button from "../../components/Button";
 
 function App() {
-  
-
-
   const quadroVazio = Array(9).fill("");
 
-  const [quadro, setQuadro] = useState(quadroVazio)
-  const [jogadorAtual, setJogadorAtual] = useState("O")
-  const [vencedor, setVencedor] = useState(null)
+  const [quadro, setQuadro] = useState(quadroVazio);
+  const [jogadorAtual, setJogadorAtual] = useState("O");
+  const [vencedor, setVencedor] = useState(null);
 
-const handleCelulaClick = (index) => {
+  const handleCelulaClick = (index) => {
+    if (vencedor) {
+      console.log("Jogo finalizado.");
+      return null;
+    }
 
-  if (vencedor) {
-    console.log("Jogo finalizado.")
-    return null;
-  }
+    if (quadro[index] !== "") {
+      return null;
+    }
 
-  if(quadro[index] !== "") {
-    return null;
-  }
+    quadro[index] = jogadorAtual;
 
-  quadro[index] = jogadorAtual;
-
-  setJogadorAtual(jogadorAtual === "X" ? "O" : "X");
-  checarVencedor();
-}
+    setJogadorAtual(jogadorAtual === "X" ? "O" : "X");
+    checarEmpate();
+    checarVencedor();
+  };
 
   const checarVencedor = () => {
     const possibilidadesDeVencer = [
@@ -43,72 +40,72 @@ const handleCelulaClick = (index) => {
       [quadro[2], quadro[4], quadro[6]],
     ];
 
-    possibilidadesDeVencer.forEach(celulas => {
-      if (celulas.every(celula => celula === "O")){
-        let pontuacao = parseInt(localStorage.getItem("jogador1")) 
+    possibilidadesDeVencer.forEach((celulas) => {
+      if (celulas.every((celula) => celula === "O")) {
+        let pontuacao = parseInt(localStorage.getItem("jogador1"));
         setVencedor("O");
         localStorage.setItem("jogador1", pontuacao + 1);
-      } 
-      if (celulas.every(celula => celula === "X")){
-        let pontuacao = parseInt(localStorage.getItem("jogador2")) 
+      }
+      if (celulas.every((celula) => celula === "X")) {
+        let pontuacao = parseInt(localStorage.getItem("jogador2"));
         setVencedor("X");
         localStorage.setItem("jogador2", pontuacao + 1);
-      } 
+      }
     });
-
-    checarEmpate();
-  }
+  };
 
   const checarEmpate = () => {
-    if (vencedor){
-      if (quadro.every(item => item !== "")) {
-        setVencedor("E");
-      }
+    if (quadro.every((item) => item !== "")) {
+      setVencedor("E");
     }
-  }
-
-  useEffect(checarVencedor, [quadro]);
+  };
 
   const resetarJogo = () => {
     setJogadorAtual("O");
-    setQuadro(quadroVazio)
+    setQuadro(quadroVazio);
     setVencedor(null);
-  }
+  };
 
   return (
-    <section className='main'>
+    <section className="main">
       <header className="board_cabecalho">
-        <Button caminho="/" texto="Voltar"/>
-        <h1 className='titulo'>Jogo da Velha</h1>
-        <Button caminho="/game" texto="Reiniciar"/>
+        <Button caminho="/" texto="Voltar" />
+        <h1 className="titulo">Jogo da Velha</h1>
+        {vencedor ? (
+          <div style={{width: '100px'}}/>
+        ) : (
+          <div onClick={() => setQuadro(quadroVazio)}>
+            <Button caminho="/game" texto="Reiniciar" />
+          </div>
+        )}
       </header>
 
       <div className={`quadro ${vencedor ? "game-over" : ""}`}>
         {quadro.map((item, index) => (
           <div
-           key={index}
-           className={`celula ${item}`}
-           onClick={() => handleCelulaClick(index)}
+            key={index}
+            className={`celula ${item}`}
+            onClick={() => handleCelulaClick(index)}
           >
             {item}
           </div>
-        ))}  
+        ))}
       </div>
-      
-      {vencedor &&
-      <footer>
-        {vencedor === "E" ?
-          <h2 class="mensagem-vencedor">
-            <span className={vencedor}>Empatou!</span>
-          </h2>
-        :
-          <h2 class="mensagem-vencedor">
-            <span className={vencedor}>{vencedor}</span> venceu!
-          </h2>
-        }
-        <button onClick={resetarJogo}>Recomeçar jogo!</button>
-      </footer>
-      }
+
+      {vencedor && (
+        <footer>
+          {vencedor === "E" ? (
+            <h2 class="mensagem-vencedor">
+              <span className={vencedor}>Empatou!</span>
+            </h2>
+          ) : (
+            <h2 class="mensagem-vencedor">
+              <span className={vencedor}>{vencedor}</span> venceu!
+            </h2>
+          )}
+          <button onClick={resetarJogo}>Recomeçar jogo!</button>
+        </footer>
+      )}
     </section>
   );
 }
